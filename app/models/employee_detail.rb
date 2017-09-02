387249a1e23bbe4649337009056a2ab9afe78712
Malarvizhi <malarvizhibe@gmail.com>
2017-09-02 21:10:19 +0530

    Update validation

--- app/models/employee_detail.rb
+++ app/models/employee_detail.rb
@@ -1,6 +1,6 @@
 class EmployeeDetail < ApplicationRecord
   validates :Name, presence: true
   validates_numericality_of :Age, :greater_than => 18 
-  validates :Gender, presence:true
-  validates :Role, presence:true
+  #validates :Gender, presence:true
+  #validates :Role, presence:true
 end
